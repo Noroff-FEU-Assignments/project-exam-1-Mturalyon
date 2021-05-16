3c561eac031b86f03bfeca9b7a1375066a0b850f
Marius Sæteraas <71336754+Mturalyon@users.noreply.github.com>
2021-05-17 00:16:01 +0200

    finnished website

--- js/script.js
+++ js/script.js
@@ -1,7 +1,75 @@
+const slider = document.querySelector(".slider");                                           //targeting the sliders which crosses boundaries with its parent that have overflow hidden
+const sliderProducts = document.querySelector(".slider_products");
+
+const url = "https://saeteraas.one/home-bar/wp-json/wp/v2/posts?per_page=100&_embed";       //tageting URL with posts
+
+async function callApi() {                                                                  //function with try catch error handling
+
+    try {
+        const response = await fetch(url);                                                  //extracting JSON from the URL
+        const json = await response.json();
+        console.log(json);
+
+        slider.innerHTML = '';                                                              //resetting containers
+        sliderProducts.innerHTML = '';
+
+        createHtml(json);                                                                   //function that creates the HTML
+    }
+    catch (error) {
+        slider.innerHTML = `
+                                        <div class="error-container">
+                                            <div>
+                                                <i class="fas fa-exclamation-circle fa-3x"></i>
+                                                <h3>WOOPS! There seems to be an error</h3>
+                                            </div>
+                                        </div>`;
+        sliderProducts.innerHTML = `
+                                        <div class="error-container">
+                                            <div>
+                                                <i class="fas fa-exclamation-circle fa-3x"></i>
+                                                <h3>WOOPS! There seems to be an error</h3>
+                                            </div>
+                                        </div>`;
+    }
+};
+
+callApi()
+
+function createHtml(json) {                                                                 //looping through the JSON adding 4 objects on each container, separating with categories
+
+    for (let i = 0; i < json.length; i++) {
+        if (json[i].categories[0] === 2) {
+            slider.innerHTML += `
+            <a href="/html/recipe-specific.html?id=${json[i].id}" class="recipe-anchor">
+                <div class="recipe-card">
+                    <img src="${json[i]._embedded["wp:featuredmedia"][0].source_url}" alt="" class="recipe-picture">
+                    <div class="card-info">
+                        <h3>${json[i].title.rendered}</h3>
+                    </div>
+                </div>
+            </a>`;
+        }
+        else if (json[i].categories[0] === 4) {
+            sliderProducts.innerHTML += `
+            <a href="/html/equipment-specific.html?id=${json[i].id}" class="recipe-anchor">
+                <div class="recipe-card">
+                    <img src="${json[i]._embedded["wp:featuredmedia"][0].source_url}" alt="" class="recipe-picture">
+                    <div class="card-info">
+                        <h3>${json[i].title.rendered}</h3>
+                    </div>
+                </div>
+            </a>`;
+        }
+        else if (i === 4) {
+            break;
+        };
+    };
+};
+
+
 //carousel Posts
 const leftBtn = document.querySelector(".btn-left");                        //targeting btns
 const rightBtn = document.querySelector(".btn-right");
-const slider = document.querySelector(".slider");                           //targeting the slider which crosses boundaries with its parent that have overflow hidden
 
 let index = 0;
 
@@ -18,7 +86,6 @@ leftBtn.addEventListener('click', function () {
 //carousel Products
 const leftBtnProducts = document.querySelector(".btn-left_products");       //had to do the same on both different carousels.
 const rightBtnProducts = document.querySelector(".btn-right_products");
-const sliderProducts = document.querySelector(".slider_products");
 
 let index2 = 0;
 
